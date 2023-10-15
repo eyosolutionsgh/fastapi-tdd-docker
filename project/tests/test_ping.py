628348dcfb651dc123479646515e14fe176d91ab
Alex <aaodoom@gmail.com>
2023-10-15 20:06:59 +0000

    initial commit

---
+++ project/tests/test_ping.py
@@ -0,0 +1,11 @@
+# following the Given-When-Then framework
+def test_ping(test_app):
+    # Given
+    # test_app
+
+    # When
+    response = test_app.get("/ping")
+
+    # Then
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong!", "environment": "dev", "testing": True}
